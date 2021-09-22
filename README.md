3a7014420817b1008c2f068bf2d1706d5928ab4b
Noor-laptob <noureldinelsherbiny5@gmail.com>
2021-09-22 20:12:49 +0200

    Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+#University Chat-Bot with RiveScript
+
+Prerequisite
+
+1. Clone this repo with git clone < github link >
+2. Open a command line and type: python -m venv env to create a virtual env
+3. Activate virtual env with source env/bin/activate.
+4. Go inside the Chat-Bot directory.
+5. Run pip install -r requirements.txt
+
+
+How to run.
+
+1. Inside of Chat-Bot dir type in terminal/console python bot.py.
+2. Click on link from the output.
+3. Hit run button to give instruction to the bot.
+4. If you want another type of conversation you can paste/create
+some Rivescript code and paste in left side on ui. 
---
+++ uni_bot/default.py
@@ -0,0 +1,66 @@
+
+DEFAULT_SOURCE = """
++ hello
+- hello
+- hi
+
++ hi
+- hello
+- Hi
+
++ what is your name
+- my name is jarvas and you
+
++ my name is *
+- <set name=<formal>>Nice to meet you, <get name>.
+
++ *
+- I don't have a reply for that.
+- Try asking that a different way.
+
++ what is the majors in the university
+- Engineering management / 
+^ Mechanical engineering / 
+^ Electrical/electronic engineering / 
+^ Civil engineering / 
+^ Computer engineering.
+
++ what is the least amount of credit hours to graduate
+- you must have 270 credit hours to graduate
+
++ how many elective courses allowed in a single semseter
+- there is no limit on the elective courses that you can get in a single semester
+
++ what is the fees of the year
+- it is a average of 50k per year but it depends on the number of credit hours you had
+
++ how much is the cost of a 1 credit hour
+- the cost of 1 credit hour is 2k
+
++ how many elective courses in the univercity
+- the university offers 15 elective course
+
++ what is the number of specialized courses in computer engineering major
+- the number of courses is 45 courses
+
++ is there a specialaization in the copmuter major
+- yes the computer engineering major has 4 submajors
+
++ what is the computer major submajors
+- the submajors are: 1. Software 2. multimedia 3. data mining 4. Mobile computing
+
++ how much does the books cost
+- the books cost 3k per year
+
++ where can i apply to the university
+- you can apply in the students affair in the university main building
+
++ what is the work time of the students affiar
+- the student affair works from 9 am to 3 pm for sunday to thursday
+
++ what is the required paper
+- the paper required is your high school certifecate and 6 personal photos and a copy of your id
+
++ where can i pay the fees of the semester
+- you can pay by your credit card or by using fawery to pay the fees
+"""
